1a5c57f770f39836c16f96ac3646e3f5faba58e1
Nakul Pradip Indurkar <nakulindurkar3@gmail.com>
2026-06-12 00:39:28 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0; 
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]", "");
+        int r = s.length()-1;
+        while(l <= r)
+        {
+            if(s.charAt(l) != s.charAt(r))
+            {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
