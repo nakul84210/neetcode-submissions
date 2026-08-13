b8ab412bedfba66c6c064314762955d587fce9e6
Nakul Pradip Indurkar <nakulindurkar3@gmail.com>
2026-08-14 00:57:27 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for(int i=0; i<nums.length; i++)
+        {
+            int cnt = 0;
+            for(int j=0; j<nums.length; j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    cnt++;
+                }
+            }
+            if(cnt >= 2)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
